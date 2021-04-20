ab6d3ec90f5c7ea37f0b4e9675aee17efdcdc4f7
beast <admin@beast.ru>
2021-04-20 13:56:05 +0300

    First homework 2021_04_20

--- squaregenerator.go
+++ squaregenerator.go
@@ -1,9 +1,24 @@
 // Package aleshkin предназначен для выполнения заданий ItUniver
 package aleshkin
 
+import (
+	"log"
+	"math"
+)
+
 // Функция SolutionSquareGenerator выполняет генерацию квадратов n натуральных чисел, начиная со start
 func SolutionSquareGenerator(start int, n int) []int {
 	var square []int
+	max := int(math.Sqrt(math.MaxInt64))
+	if start <= 0 {
+		log.Fatal("Заданное число не является натуральным")
+	}
+	if n < 1 {
+		log.Fatal("Количество выводимых чисел не должно быть меньше одного")
+	}
+	if start > (max - n + 1) {
+		log.Fatal("Введённый диапазон чисел выходит за границы int")
+	}
 	for i := start; i < start+n; i++ {
 		square = append(square, i*i)
 	}
---
+++ zerosequence.go
@@ -0,0 +1,34 @@
+// Package aleshkin предназначен для выполнения заданий ItUniver
+package aleshkin
+
+import "math"
+
+// Функция SolutionBinaryGap находит самую длинную последовательность нулей в двоичном представлении числа N
+func SolutionBinaryGap(N int) int {
+	var num uint
+	max := 0
+	seqlen := 0
+	if N == 0 {
+		max = 1
+	}
+	if N > 0 {
+		num = uint(N)
+	}
+	if N < 0 {
+		num = math.MaxUint64 - uint(math.Abs(float64(N))) + 1
+	}
+	for num > 0 {
+		if num%2 == 0 {
+			seqlen++
+			num = num / 2
+		}
+		if num%2 == 1 {
+			if seqlen > max {
+				max = seqlen
+			}
+			seqlen = 0
+			num = num / 2
+		}
+	}
+	return max
+}
