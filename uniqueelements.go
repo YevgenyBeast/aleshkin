c048cb7bb71d0ac4c42acadb8a0d8c8aacbc0542
beast <admin@beast.ru>
2021-04-21 19:04:27 +0300

    Second homework 2021_04_21

--- uniqueelements.go
+++ uniqueelements.go
@@ -2,16 +2,10 @@
 package aleshkin
 
 // Функция SolutionUniq выполняет подсчёт количества уникальных значений в массиве A
-func SolutionUniq(A []int) int {
-	var unique int = 0
+func SolutionUniq1(A []int) int {
 	counts := make(map[int]int)
 	for _, number := range A {
 		counts[number]++
 	}
-	for _, count := range counts {
-		if count == 1 {
-			unique++
-		}
-	}
-	return unique
+	return len(counts)
 }
